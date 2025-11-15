30e6fb5f16fc47ef794f1527db198fa730e32524
NickKipshidze <kipshidze.nick@gmail.com>
2025-11-15 14:59:44 +0400

    Add dataset get endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import os
 import flask
 import flask_cors
+import json
 import dotenv
 
 dotenv.load_dotenv(dotenv_path=".env")
@@ -11,6 +12,17 @@ flask_cors.CORS(app, origins=["*"])
 @app.route("/", methods=["GET"])
 def root():
     return flask.Response(f"<pre>{open("docs.txt", "r").read()}</pre>", status=200)
+
+@app.route("/dataset/get", methods=["GET"])
+def get_data():
+    dataset_name = flask.request.args.get("name")
+    dataset_path = os.path.join(os.getenv("DATASET_ROOT"), dataset_name+".json")
+
+    if os.path.exists(dataset_path):
+        indexation = json.load(open(dataset_path, "r"))
+        return flask.jsonify(indexation), 200
+    else:
+        return flask.jsonify({"error": f"Dataset \"{dataset_name}\" not found."}), 404
 
 if __name__ == "__main__":
     app.run(
